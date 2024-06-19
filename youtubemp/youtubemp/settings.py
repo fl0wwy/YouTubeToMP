d08abfdfc9beaf5f6d50e511ad137a92af527518
bar <psnoob510@gmail.com>
2024-06-19 20:59:38 +0300

    adding host

--- youtubemp/youtubemp/settings.py
+++ youtubemp/youtubemp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['flowwy.pythonanywhere.com']
 
 
 # Application definition
